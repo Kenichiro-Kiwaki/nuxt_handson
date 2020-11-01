9a33910037c397d61809aef36ec0689f5b798ba5
keni1211 <k.auction1211@gmail.com>
2020-11-01 16:46:30 +0900

    todoリスト機能の実装

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,63 @@
   <div class="container">
     <p>
       user: {{ $store.getters.getUserName }}
+    </p><br>
+    <button class="button is-primary is-rounded" @click="login">
+      ログイン
+    </button><br>
+    <table class="table is-narrow">
+      <thead>
+        <tr>
+          <th>todo</th>
+          <th>limit</th>
+       </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in $store.getters.getTodos" :key="todo.todo">
+          <td>{{todo.todo}}</td>
+          <td>{{todo.limit}}</td>
+        </tr>
+      </tbody>
+   </table><br>
+   <div class="field is-grouped">
+    <p class="control is-expanded">
+      <input v-model="newTodo" class="input" type="text" placeholder="todo">
     </p>
-      <button class="button is-primary is-rounded" @click="login">
-        ログイン
-      </button>
+    <p class="control is-expanded">
+      <input v-model="newLimit" class="input" type="text" placeholder="limit">
+    </p><br>
+    <p class="control">
+      <a class="button is-primary" @click="addTodo">
+        add
+      </a>
+    </p>
+   </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods:{
+  data() {
+    return {
+      newTodo: '',
+      newLimit: ''
+    }
+  },
+  methods: {
     login() {
       this.$store.dispatch('login')    
+    },
+    addTodo() {
+      const todo = this.newTodo
+      const limit = this.newLimit
+
+      this.$store.dispatch('addTodo', {todo, limit});
+      this.newTodo = '',
+      this.newLimit = ''
     }
+  },
+  created() {
+    this.$store.dispatch('fetchTodos')
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,19 +1,26 @@
 import firebase from '~/plugins/firebase'
+const db = firebase.firestore();
+const todoRef = db.collection('todos')
 
 export const state = () => ({
     userUid: '',
-    userName: ''
+    userName: '',
+    todos:[]
 })
 
 export const mutations = {
     setUserUid(state, userUid) {
         state.userUid = userUid
-        console.log('Uid is SET!')
     },
     setUserName(state, userName) {
         state.userName = userName
-        console.log('userName is SET!')
     },
+    addTodos(state, todo) {
+        state.todos.push(todo)
+    },
+    clearTodo(state, todo) {
+        state.todos = []
+    }
 }
 
 export const actions = {
@@ -22,23 +29,41 @@ export const actions = {
     const provider = new firebase.auth.TwitterAuthProvider()
     firebase.auth().signInWithPopup(provider).then(function (result) {
         const user = result.user;
-        console.log(user)
-        console.log(user.displayName)
-        // dispatch('checkLogin')
         commit('setUserUid', user.uid)
         commit('setUserName', user.displayName)
     }).catch(function (error) {
+        var errorCode = error.code;
+        console.log('error : ' + errorCode)
     })    
   },
-//   checkLogin() {
-//     firebase.auth().onAuthStatusChanged(function (user) {
-//         if (user) {
-//             console.log('log-in success!')
-//         } else {
-//             console.log('failed')
-//         }
-//     })
-//   }
+  fetchTodos({ commit }) {
+    todoRef
+    .get()
+    .then(res => {
+        res.forEach((doc) => {
+            console.log('success' + `${doc.id} => ${doc.data()}`);
+            commit('addTodos', doc.data())
+        })
+    })
+    .catch(error => {
+        console.log('error :' + error)
+    })
+  },
+  addTodo({commit}, todo) {
+      console.log(todo)
+      todoRef
+      .add({
+          todo: todo.todo,
+          limit: todo.limit,
+      })
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        commit('addTodos', todo)
+      })
+      .catch((error) => {
+          console.error('Error adding document :' , error)
+      })
+  }
 }
 
 export const getters = {
@@ -47,5 +72,8 @@ export const getters = {
     },
     getUserName(state) {
         return state.userName
-    }    
+    },
+    getTodos(state) {
+        return state.todos
+    }
 }
